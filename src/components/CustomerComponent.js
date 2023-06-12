870dbf8a2ca41624c9291e9d70fbf2fbd1db6483
Sarah Abdulle <sarahabdulle1@yahoo.com>
2023-06-12 13:52:57 +0100

    added enter location functionality to customer component

--- src/components/CustomerComponent.js
+++ src/components/CustomerComponent.js
@@ -1,29 +1,65 @@
 import {useState, useEffect} from "react";
 
-
 const CUSTOMER_SERVER_URL = "http://localhost:8080/customer";
-const CustomerComponent = () => {
+
+const CustomerComponent = ({customer}) => {
+
+    // container
+    // const [customers, setCustomers] = useState({});
+
+    // component 
     const [customerLocation, setCustomerLocation]= useState("");
 
-    const getCustomerLocation = async (location) => {
-        const response = await fetch (`${CUSTOMER_SERVER_URL}/{customer.customerId}/location`)
+    // const getCustomer = async () => {
+    //     const response = await fetch(`${CUSTOMER_SERVER_URL}`,{
+    //         method: "GET",
+    //         headers:{"Content-Type": "application/json"} 
+    //     })
+    //     const allCustomers = await response.json();
+    //     const currentCustomer = allCustomers[0];
+    //     setCustomers(currentCustomer);
+    // }
+
+    const getCustomerLocation = async (customerId) => {
+        const response = await fetch (`${CUSTOMER_SERVER_URL}/${customer.customerId}/location`,{
+            method: "GET",
+            headers:{"Content-Type": "application/json"}
+        })
+        const customerLocationData = await response.json();
+        setCustomerLocation(customerLocationData);
     }
-    const addCustomerLocation = async (location) => {
-        const response = await fetch (`${CUSTOMER_SERVER_URL}/{customer.customerId}/location/edit`)
+    const addCustomerLocation = async (newLocation) => {
+        const response = await fetch (`${CUSTOMER_SERVER_URL}/${customer.customerId}/location/edit`, {
+            method : "PATCH",
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify({location: newLocation}),
+        })
+        const newCustomerLocation = await response.json();
+        setCustomerLocation(newCustomerLocation); 
+    
     }
+
+//     const handleFormSubmit = (event) => {
+//     event.preventDefault();
+//     customer()
+//   };
+
+    const handleLocationChange = (event) => {
+    setCustomerLocation(event.target.value);
+    };
 
     useEffect(() => {
         getCustomerLocation()
     }, [])
     return ( <>
             <h2>Hello CustomerComponent</h2>
-            <form onSubmit={handleFormSubmit}>
+            <form >
                 <input 
                 type="text"
                 name="customerLocation"
                 placeholder="Enter your current location"
-                value ={setCustomerLocation}
-                onChange={handleChange}
+                value ={customerLocation}
+                onChange={handleLocationChange}
                 />
                 <button type="submit"> Enter</button>
 
--- src/containers/RestaurantContainer.js
+++ src/containers/RestaurantContainer.js
@@ -1,6 +1,26 @@
+import {useState, useEffect} from "react";
+import CustomerComponent from "../components/CustomerComponent";
+
+const CUSTOMER_SERVER_URL = "http://localhost:8080/customer";
+
 const RestaurantContainer = () => {
+    const [customers, setCustomers] = useState({});
+
+
+    const getCustomer = async () => {
+        const response = await fetch(`${CUSTOMER_SERVER_URL}`,{
+            method: "GET",
+            headers:{"Content-Type": "application/json"} 
+        })
+        const allCustomers = await response.json();
+        const currentCustomer = allCustomers[0];
+        setCustomers(currentCustomer);
+    }
+
     return ( <>
+    
             <h1>Hello From RestaurantContainer</h1>
+            <CustomerComponent customer={customers}/>
     </> );
 }
  
