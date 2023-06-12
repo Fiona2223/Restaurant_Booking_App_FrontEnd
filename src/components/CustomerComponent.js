4ee237be89e28de9685522bebd2f0890fb4e202e
Sarah Abdulle <sarahabdulle1@yahoo.com>
2023-06-12 13:38:44 +0100

    added to customer component

--- src/components/CustomerComponent.js
+++ src/components/CustomerComponent.js
@@ -1,6 +1,33 @@
+import {useState, useEffect} from "react";
+
+
+const CUSTOMER_SERVER_URL = "http://localhost:8080/customer";
 const CustomerComponent = () => {
+    const [customerLocation, setCustomerLocation]= useState("");
+
+    const getCustomerLocation = async (location) => {
+        const response = await fetch (`${CUSTOMER_SERVER_URL}/{customer.customerId}/location`)
+    }
+    const addCustomerLocation = async (location) => {
+        const response = await fetch (`${CUSTOMER_SERVER_URL}/{customer.customerId}/location/edit`)
+    }
+
+    useEffect(() => {
+        getCustomerLocation()
+    }, [])
     return ( <>
             <h2>Hello CustomerComponent</h2>
+            <form onSubmit={handleFormSubmit}>
+                <input 
+                type="text"
+                name="customerLocation"
+                placeholder="Enter your current location"
+                value ={setCustomerLocation}
+                onChange={handleChange}
+                />
+                <button type="submit"> Enter</button>
+
+            </form>
     </> );
 }
  
