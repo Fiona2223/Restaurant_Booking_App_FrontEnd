707756a7bb05d042e1c42c8a4295f9eeb7e9786e
Taliane Moussassa <talianegloire392@gmail.com>
2023-06-12 12:48:13 +0100

    rendering profiles

--- src/components/RestaurantProfileListComponent.js
+++ src/components/RestaurantProfileListComponent.js
@@ -1,6 +1,11 @@
-const RestaurantProfileListComponent = () => {
+import RestaurantProfileComponent from "./RestaurantProfileComponent";
+
+const RestaurantProfileListComponent = ({listOfRestaurants}) => {
+    const profileList = listOfRestaurants.map((restaurant)=> {
+        return <RestaurantProfileComponent key={restaurant.id} restaurant={restaurant} />
+    }) 
     return ( <>
-    
+        {profileList}
      </> );
 }
  
