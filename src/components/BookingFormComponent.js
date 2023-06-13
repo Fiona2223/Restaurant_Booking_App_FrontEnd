16f159dde899b8659f71b17f93dac0bd8589dc40
Yasmin haidar <yasmin1635@hotmail.com>
2023-06-13 18:02:24 +0100

    Make reservation book created

--- src/components/BookingFormComponent.js
+++ src/components/BookingFormComponent.js
@@ -7,12 +7,18 @@ import TableListComponent from "../components/TableListComponent";
 import { useState, useEffect } from "react";
 import DateTimePicker from 'react-datetime-picker';
 import DatePicker from 'react-datepicker';
+import TimePicker from 'react-time-picker';
+import PickTableComponent from "./PickTableComponent";
+
+
 const BookingFormComponent = ({restaurant}) => {
 
     const [tableList, setTableList] = useState([]);
     const [showAllTables , setShowAllTables] = useState(true);
     const [allAvailableTables, setAllAvailableTables] = useState([]);
     const [startDate,setStartDate] = useState(new Date());
+    const [value, onChange] = useState('10:00');
+    const [showPickTableComponent, setShowPickTableComponent] = useState(false);
     
 
     const fetchTablesByRestaurantId = async () => {
@@ -27,6 +33,10 @@ const BookingFormComponent = ({restaurant}) => {
 
     const handleShowwAllTables = (event) => {
         setShowAllTables(true);   
+    }
+
+    const handleMakeReservation = (event) => {
+        setShowPickTableComponent(true);
     }
 
 // tables with no booking ids should be displayed when clicking available_tables
@@ -52,14 +62,22 @@ const BookingFormComponent = ({restaurant}) => {
                 <button onClick={handleShowAvailableTables}>Available Tables</button>
             </div>
             <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}/>
+            <TimePicker onChange={onChange} value={value} />
             {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <TimePicker label="Basic time picker" />
                 <DatePicker label="Basic date picker" />
             </LocalizationProvider> */}
-            <div>
+            {/* <div>
                 <TableListComponent tableList={tableList} allAvailableTables={allAvailableTables} showAllTables={showAllTables}/>
+            </div> */}
+            <div>
+                {showPickTableComponent ? <PickTableComponent /> :  <div>
+                <TableListComponent tableList={tableList} allAvailableTables={allAvailableTables} showAllTables={showAllTables}/></div> 
+                }
+            <button onClick={handleMakeReservation}>Make Reservation</button>
+
             </div>
-            <button>Make Reservation</button>
+            
     </> );
 }
  
