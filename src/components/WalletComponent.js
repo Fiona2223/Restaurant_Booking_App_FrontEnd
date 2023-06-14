914076ff3c37538ddb9e3fc5c047e29d104667ad
Sarah Abdulle <sarahabdulle1@yahoo.com>
2023-06-14 10:09:28 +0100

    made minor changes to wallet

--- src/components/WalletComponent.js
+++ src/components/WalletComponent.js
@@ -1,11 +1,14 @@
 const WalletComponent = ({customerBookings, booking}) => {
     // console.log(customerBookings);
+
+    // TO DO: change the format of booking.tableIds from array to string
+
     return (  
         <>  
         <ul>
-            <li> You have a reservation at {booking.restaurantName} on {booking.date} at {booking.time}</li>
+            <li> You have a reservation at {booking.restaurantName} on {booking.date} at {booking.time}.</li>
             <li> The tables you have reserved are {booking.tablesIds}.</li>
-            <li>Your booking ID is {booking.id}. Please present this upon arrival.</li>
+            <li> Your booking ID is {booking.id}. Please present this upon arrival.</li>
         </ul>
         </>
     );
